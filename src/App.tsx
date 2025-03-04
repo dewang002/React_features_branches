fde6f3f285a32eb9b89a36bbb3b9019c4e846f4f
dewang002 <2002thapa@gmail.com>
2025-03-04 13:42:16 +0530

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 
-const category = [
-  "Fruit",
-  "Vegitable",
-  "Sweets"
-]
+const App = () => {
 
-const items: Record<string, string[]> = {
-  Fruit:["apple","orange","mango"],
-  Vegitable:["broccli","cabage","pumpkin"],
-  Sweets:["donuts","orio","ice-cream"]
-}
-
-
-function App() {
-
-  const [categories, setCategories] = useState('')
-  const [subcategories, setSubCategories] = useState('')
-  const [isEnable, setIsEnable] = useState(false)
-  useEffect(()=>{
-    console.log(categories,subcategories)
-  },[categories,subcategories])
+  
+  const fetchData = async() => {
+      const res = await fetch(`https://dummyjson.com/users/5/posts`)
+      const data = await res.json()
+      console.log(data)
+      return data
+  }
+  
+  useEffect(()=>{ 
+    fetchData()
+  })
   return (
-   <>
+    <div>
 
-   <select value={categories} onChange={(e)=>{
-    setCategories(e.target.value)
-    setSubCategories("")
-    setIsEnable(true)
-    }}>
-     <option value="">value</option>
-    {
-      category.map((elem,index) => (
-        <option key={index} value={elem} >{elem}</option>
-      ))
-    }
-   </select>
-    <br />
-
-   <select id="" onChange={(e)=>setSubCategories(e.target.value)} value={subcategories} disabled={!isEnable}>
-    <option value="">value</option>
-    {isEnable && items[categories].map((item,index)=>(
-      <option key={index} value={item}>{item}</option>
-    ))}
-   </select>
-   </>
+    </div>
   )
 }
 
