5c46b05e66aae5cbb6268fc76f82d0fc9dfbfa2e
dewang002 <2002thapa@gmail.com>
2025-03-04 14:46:08 +0530

    setTimeout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import React, {  useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [,setNumber] = useState({})
-  const number = useRef(0)
-  const handleClick = () =>{
-    number.current-=1;
-    setNumber({})
-  }
-  const handleAdd = () =>{
-    number.current+=1;
-    setNumber({})
-  }
+  const [val,setVal] = useState("Hello World")
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      setVal("goodBuy world")
+    },5000)
+    return ()=> clearTimeout(timer)
+  },[val])
   return (
-    <div className='h-screen gap-4 flex items-center justify-center'>
-    <span onClick={handleClick} className=' decrease text-xl'>-</span>
-    <h1 className='number'>{number.current}</h1>
-    <span onClick={handleAdd} className=' increase text-xl'>+</span>
-    </div>
+    <div>{val}</div>
   )
 }
 
