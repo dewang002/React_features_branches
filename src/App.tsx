4c57072abc5c4e850706834c165a753e2f676187
dewang002 <2002thapa@gmail.com>
2025-03-12 12:48:49 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import { useState } from "react"
-
-
+import { memo, useCallback, useState } from "react"
 
 const App = () => {
-  const [dark, setDark] = useState(false)
+  const [fields, setFields] = useState([{ id: 1, value: '' }])
+
+  const handleChange = useCallback((id:number,value:string) => {
+  const updatedField =  fields.map(items => (
+    items.id===id ? {...items, value:value} : items
+  ))
+  setFields(updatedField)
+  }, [fields])
+
+  const handleAdd = () => {
+    const newFiled = {id: fields.length+1, value:''}
+    setFields([...fields, newFiled])
+  }
+
+  const handleDelete = (id:number) => {
+    const newdata = fields.filter(item => item.id !== id)
+    if(newdata){
+      setFields(newdata)
+    }
+  }
+
   return (
-    <div className={dark&&'dark'}>
-    <div  className="bg-white  h-screen w-full dark:bg-black  px-6 py-8 ring shadow-xl ring-gray-900/5">
-  <div>
-    <span onClick={() => setDark(prev=>!prev)}  className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
-      <svg  className="h-6 w-6 stroke-white">
-        </svg>
-        </span>
-  </div>
-  <h3  className="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
-  <p  className="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-  </p>
-</div>
+    <>
+      {fields.map(text => (
+        <div key={text.id}>
+          <Input onchange={(e:any) => handleChange(text.id, e.target.value)} value={text.value} />
+          <button onClick={()=>handleDelete(text.id)}>delete</button>
         </div>
+      ))}
+      <Button />
+      <button onClick={handleAdd}>ADD</button>
+    </>
   )
 }
+
+const Input = ({ onchange, value }) => {
+  return (
+    <>
+      <input className="border" type="text" value={value} onChange={onchange} />
+    </>
+  )
+}
+
+const Button = memo(() => {
+  console.log('btn compo')
+  const [count, setCount] = useState(0)
+  return (
+    <button className="bg-red-300 p-3" onClick={() => setCount(prev => prev + 1)}>{count}</button>
+  )
+})
 
 export default App
